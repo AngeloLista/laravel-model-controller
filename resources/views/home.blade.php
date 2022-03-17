fbfa3eac09f7443ecbc3e79dbe533a31bbc31fba
AngeloLista <abstractplayer@gmail.com>
2022-03-17 21:57:17 +0100

    fixed calling mysql db syntax

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -15,15 +15,15 @@
             <div class="card">
                 {{-- TITLES --}}
                 <div class="card-title-container">
-                    <h5 class="card-title">{{ $movie['title'] }}</h5>
-                    <h6 class="card-subtitle">{{ $movie['original_title'] }}</h6>
+                    <h5 class="card-title">{{ $movie->title }}</h5>
+                    <h6 class="card-subtitle">{{ $movie->original_title }}</h6>
                 </div>
                 {{-- INFOS --}}
                 <div>
                     <div class="card-info-container">
-                        <h5 class="card-info">Nationality: <span>{{ $movie['nationality'] }}</span></h5>
-                        <h5 class="card-info">Date: <span>{{ $movie['date'] }}</span></h5>
-                        <h5 class="card-info">Vote: <span>{{ $movie['vote'] }}</span></h5>
+                        <h5 class="card-info">Nationality: <span>{{ $movie->nationality }}</span></h5>
+                        <h5 class="card-info">Date: <span>{{ $movie->date }}</span></h5>
+                        <h5 class="card-info">Vote: <span>{{ $movie->vote }}</span></h5>
                     </div>
                     {{-- BUTTON --}}
                     <div class="btn-container">
