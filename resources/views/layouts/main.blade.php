a1b3982c76983cf537950622b3a49f04b6ea9888
AngeloLista <abstractplayer@gmail.com>
2022-03-17 23:43:24 +0100

    added main layout

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -5,10 +5,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home</title>
+
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+
 </head>
 <body>
     <main>
         @yield('content')
     </main>
+
+    <script src="{{ asset('js/app.js') }}"></script>
 </body>
 </html>
--- resources/views/movies/show.blade.php
+++ resources/views/movies/show.blade.php
@@ -1,36 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>{{$movie->title}}</title>
+@extends('layouts.main')
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-</head>
-<body>
-    <main>
-        <div class="card-container">
-            <div class="card">
-                {{-- TITLES --}}
-                <div class="card-title-container">
-                    <h5 class="card-title">{{ $movie->title }}</h5>
-                    <h6 class="card-subtitle">{{ $movie->original_title }}</h6>
-                </div>
-                {{-- INFOS --}}
-                <div>
-                    <div class="card-info-container">
-                        <h5 class="card-info">Nationality: <span>{{ $movie->nationality }}</span></h5>
-                        <h5 class="card-info">Date: <span>{{ $movie->date }}</span></h5>
-                        <h5 class="card-info">Vote: <span>{{ $movie->vote }}</span></h5>
-                    </div>
-                    {{-- BUTTON --}}
-                    <div class="btn-container">
-                        <a class="discover-btn" href="{{route('movies.index')}}">Torna alla lista</a>
-                    </div>
-                </div>
+@section('content')
+<div class="card-container">
+    <div class="card">
+        {{-- TITLES --}}
+        <div class="card-title-container">
+            <h5 class="card-title">{{ $movie->title }}</h5>
+            <h6 class="card-subtitle">{{ $movie->original_title }}</h6>
+        </div>
+        {{-- INFOS --}}
+        <div>
+            <div class="card-info-container">
+                <h5 class="card-info">Nationality: <span>{{ $movie->nationality }}</span></h5>
+                <h5 class="card-info">Date: <span>{{ $movie->date }}</span></h5>
+                <h5 class="card-info">Vote: <span>{{ $movie->vote }}</span></h5>
+            </div>
+            {{-- BUTTON --}}
+            <div class="btn-container">
+                <a class="discover-btn" href="{{route('movies.index')}}">Torna alla lista</a>
             </div>
         </div>
-    </main>
-</body>
-</html>
+    </div>
+</div>
+@endsection
