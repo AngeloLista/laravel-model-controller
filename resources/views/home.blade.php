c2bc996f54f82b9a949279f263bd54e7c2e28936
AngeloLista <abstractplayer@gmail.com>
2022-03-17 20:57:06 +0100

    created movie cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.1.3/css/bootstrap.min.css' integrity='sha512-GQGU0fMMi238uA+a/bdWJfpUGKUkBdgfFdgBm72SUQ6BeyWjoY/ton0tEjH+OSH9iP4Dfh+7HM0I9f5eR0L/4w==' crossorigin='anonymous'/>
+    
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
     <main>
-        <div class="container">
+        <div class="card-container">
             @foreach ($movies as $movie)
-                movie
+            <div class="card">
+                <h5 class="card-title">{{ $movie['title'] }}</h5>
+                <h6 class="card-subtitle">{{ $movie['original_title'] }}</h6>
+                <div class="card-infos">
+                    <span>Nationality: {{ $movie['nationality'] }}</span>
+                    <span>Date: {{ $movie['date'] }}</span>
+                    <span>Vote: {{ $movie['vote'] }}</span>
+                </div>
+                
+
+            </div>
             @endforeach
         </div>
     </main>
